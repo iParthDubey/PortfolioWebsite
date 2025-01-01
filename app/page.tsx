5aed7bb77db7d8f5ddfb1a66ba30cfd53d641632
Parth Dubey <parthdubey@Parths-MacBook-Pro.local>
2025-01-01 19:27:16 +0530

    Fix import

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 "use client"
-import { Main } from "next/document";
+// import { Main } from "next/document";
 import { useState } from "react";
 import Head from "next/head";
 import Image from "next/image";
